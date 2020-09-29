7f359b54beeb36c11c1afb2cdd2152965781a89b
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-09-28 20:58:15 -1000

    Working Puppeteer + Cleverbot

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"),
+	puppeteer = require("puppeteer");
 const client = new Discord.Client();
 
 const prefix = "yo karen "; // or "!", "-", "%"
+
 client.on("message", (message) => {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
@@ -17,6 +19,40 @@ client.on("message", (message) => {
 	}
 });
 
-client.login(process.env.BOT_TOKEN).catch((err) => {
-	console.log(err);
-});
+// client.login(process.env.BOT_TOKEN).catch((err) => {
+// 	console.log(err);
+// });
+
+(async () => {
+	console.log("Start");
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto("https://www.cleverbot.com/");
+	console.log("Page loaded");
+
+	// Send over a message
+	console.log("Sending message");
+	await page.evaluate(() => {
+		document.querySelector(".stimulus").value = "test";
+		cleverbot.sendAI();
+	});
+
+	// Wait for response
+	console.log("Awaiting response");
+	await page.waitForSelector("#snipTextIcon"); // #snipTextIcon is added to DOM when the bot finishes speaking
+
+	// Parse response
+	const response = await page.evaluate(() => {
+		return document.querySelector("#line1 .bot").textContent; // the bot always says its last message in #line1
+	});
+	console.log(response);
+
+	console.log("Closing");
+	await browser.close();
+	console.log("Closed");
+})();
+
+/*
+ * Resources
+ * https://www.digitalocean.com/community/tutorials/how-to-build-a-discord-bot-with-node-js
+ */
