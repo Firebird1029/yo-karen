593985eaae48612db4dfc314ce7c64f5db9e49dc
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-09-28 21:15:16 -1000

    Working Discord + Cleverbot

--- index.js
+++ index.js
@@ -4,25 +4,6 @@ const client = new Discord.Client();
 
 const prefix = "yo karen "; // or "!", "-", "%"
 
-client.on("message", (message) => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
-
-	// Parse Command and Arguments
-	const args = message.content.slice(prefix.length).split(" ");
-	const command = args.shift().toLowerCase();
-
-	// Commands
-	if (command === "ping") {
-		const timeTaken = Date.now() - message.createdTimestamp;
-		message.reply(`Pong! This message had a latency of ${timeTaken} ms.`);
-	}
-});
-
-// client.login(process.env.BOT_TOKEN).catch((err) => {
-// 	console.log(err);
-// });
-
 (async () => {
 	console.log("Start");
 	const browser = await puppeteer.launch();
@@ -30,29 +11,56 @@ client.on("message", (message) => {
 	await page.goto("https://www.cleverbot.com/");
 	console.log("Page loaded");
 
-	// Send over a message
-	console.log("Sending message");
-	await page.evaluate(() => {
-		document.querySelector(".stimulus").value = "test";
-		cleverbot.sendAI();
+	async function askCleverBot(request) {
+		// Send over a message
+		console.log("Sending message");
+		await page.evaluate((request) => {
+			document.querySelector(".stimulus").value = request;
+			cleverbot.sendAI(); // a form submit event triggers this API call
+		}, request); // bind request variable into evaluate function
+
+		// Wait for response
+		console.log("Awaiting response");
+		await page.waitForSelector("#snipTextIcon"); // #snipTextIcon is added to DOM when the bot finishes speaking
+
+		// Parse response
+		const response = await page.evaluate(() => {
+			return document.querySelector("#line1 .bot").textContent; // the bot always says its last message in #line1
+		});
+		console.log(response);
+	}
+
+	// Discord Code
+	// ----------
+	client.on("message", (message) => {
+		if (message.author.bot) return;
+		if (!message.content.startsWith(prefix)) return;
+
+		// Parse Command and Arguments
+		const args = message.content.slice(prefix.length).split(" ");
+		// const command = args.shift().toLowerCase();
+
+		// Commands
+		// if (command === "ping") {
+		// 	const timeTaken = Date.now() - message.createdTimestamp;
+		// 	message.reply(`Pong! This message had a latency of ${timeTaken} ms.`);
+		// }
+		askCleverBot(args);
 	});
 
-	// Wait for response
-	console.log("Awaiting response");
-	await page.waitForSelector("#snipTextIcon"); // #snipTextIcon is added to DOM when the bot finishes speaking
-
-	// Parse response
-	const response = await page.evaluate(() => {
-		return document.querySelector("#line1 .bot").textContent; // the bot always says its last message in #line1
+	client.login(process.env.BOT_TOKEN).catch((err) => {
+		console.log(err);
 	});
-	console.log(response);
+	// ----------
 
-	console.log("Closing");
-	await browser.close();
-	console.log("Closed");
+	// console.log("Closing");
+	// await browser.close();
+	// console.log("Closed");
 })();
 
 /*
  * Resources
+ * https://github.com/puppeteer/puppeteer
+ * https://gist.github.com/LeCoupa/e8b9aac95d0d7acbde740c91c4bcc178
  * https://www.digitalocean.com/community/tutorials/how-to-build-a-discord-bot-with-node-js
  */
